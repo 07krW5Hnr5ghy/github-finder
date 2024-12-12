401e6799c5943a5e8e7d144d13a9fefc85944fa2
07krW5Hnr5ghy <jryn145@gmail.com>
2024-12-12 01:28:10 -0500

    feature(main): handle button render

--- js/script.js
+++ js/script.js
@@ -1,19 +1,6 @@
-async function getRepoInfo(language){
-    const letters = "abcdefghijklmnopqrstuvwxyz";
-    const request = await fetch(`https://api.github.com/search/repositories?q=${letters[getRandomInt(letters.length-1)]}+language:${language}`);
-    const data = await request.json();
-    if(data.items.length){
-        return data.items[getRandomInt(data.items.length-1)];
-    }else{
-        return [];
-    }
-    
-}
-
-function getRandomInt(max){
-    return Math.floor(Math.random()*max);
-}
-
+const contentWrapper = document.querySelector(".github-finder-wrapper");
+const languageSelector = document.querySelector(".language-selector");
+const infoWrapper = document.querySelector(".info-loading");
 const languages = [
     {
         name:"javascript",
@@ -45,20 +32,26 @@ const languages = [
         color:"#f34b7d"
     }
 ];
-const languageSelector = document.querySelector(".language-selector");
-const infoWrapper = document.querySelector(".info-loading");
-const refreshButton = document.querySelector(".refresh-button");
 
-languages.forEach(language=>{
-    const languageOption = document.createElement("option");
-    languageOption.textContent = language.name;
-    languageSelector.appendChild(languageOption);
-});
+async function getRepoInfo(language){
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const request = await fetch(`https://api.github.com/search/repositories?q=${letters[getRandomInt(letters.length-1)]}+language:${language}`);
+    const data = await request.json();
+    if(data.items.length){
+        return data.items[getRandomInt(data.items.length-1)];
+    }else{
+        return [];
+    }
+    
+}
 
-refreshButton.addEventListener("click",async ()=>{
+function getRandomInt(max){
+    return Math.floor(Math.random()*max);
+}
+
+async function handleRepoInfo(){
     infoWrapper.classList.add("info-loading");
     infoWrapper.classList.remove("info-wrapper");
-    const loadingInfo = document.createElement("span");
     const spanName = document.createElement("span");
     const description = document.createElement("p");
     const footer = document.createElement("div");
@@ -66,12 +59,12 @@ refreshButton.addEventListener("click",async ()=>{
     const starsDiv = document.createElement("div");
     const forksDiv = document.createElement("div");
     const watchersDiv = document.createElement("div");
-    infoWrapper.innerHTML = "";
-    loadingInfo.textContent = "Loading please wait...";
-    infoWrapper.appendChild(loadingInfo);
+    infoWrapper.innerHTML = '<span>Loading please wait...</span>';
     const repoInfo = await getRepoInfo(languageSelector.value);
-    infoWrapper.removeChild(loadingInfo);
     infoWrapper.classList.remove("info-loading");
+    const refreshButton = document.createElement("button");
+    refreshButton.type = "button";
+    refreshButton.classList.add("refresh-button");
     if(repoInfo.length){
         infoWrapper.classList.add("info-wrapper");
         spanName.textContent = repoInfo.name;
@@ -103,6 +96,24 @@ refreshButton.addEventListener("click",async ()=>{
     }else{
         infoWrapper.classList.add("info-error");
         infoWrapper.textContent = "Error fetching repositories";
+        refreshButton.style.background = "red";
+        refreshButton.textContent = "Click to retry";
     }
-    
+    contentWrapper.appendChild(refreshButton);
+    refreshButton.addEventListener("click",()=>{
+        if(contentWrapper.childElementCount===3){
+            contentWrapper.removeChild(refreshButton);
+        }
+        handleRepoInfo();
+    });
+}
+
+languages.forEach(language=>{
+    const languageOption = document.createElement("option");
+    languageOption.textContent = language.name;
+    languageSelector.appendChild(languageOption);
+});
+
+languageSelector.addEventListener("change",async ()=>{
+    handleRepoInfo();
 });
